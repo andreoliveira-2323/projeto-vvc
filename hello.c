785a674b632a39d74bd8198a5db8720f963d83a9
andreoliveira-2323 <andreoliveira.aluno@unipampa.edu.br>
2025-06-19 22:35:13 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 5;
+    int resultado;
+
+    resultado = a * b;
+
+    printf("O resultado de %d x %d é %d\n", a, b, resultado);
+
+    return 0;
+}
+
